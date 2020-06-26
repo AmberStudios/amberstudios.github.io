3bdef752237676224b73737759686d09b14fa41c
Heccing Commie <51051125+Turtlz444@users.noreply.github.com>
2020-06-26 07:26:54 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var sz = screen.height*0.07;
 document.getElementById("dd").setAttribute("width", sz); 
 document.getElementById("dd").setAttribute("height", sz); 
 console.log(sz);
-var closedOrOpen = 1;
+var closedOrOpen = 0;
 document.getElementById("headerLogo").style.height = sz * 0.7;
 
 /* - dropdown button function - */
