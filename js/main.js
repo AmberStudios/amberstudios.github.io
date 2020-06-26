4e1f145f4674102357ca9dd5ca7bae9a8e3efae8
Heccing Commie <51051125+Turtlz444@users.noreply.github.com>
2020-06-25 22:59:06 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
+/* - set up - */
 var sz = screen.height*0.07;
 document.getElementById("dd").setAttribute("width", sz); 
 document.getElementById("dd").setAttribute("height", sz); 
 console.log(sz);
 var closedOrOpen = 1;
-/* document.getElementById("headerLogo").style.height = sz * 0.7;
+document.getElementById("headerLogo").style.height = sz * 0.7;
+
+/* - dropdown button function - */
 window.onload = function drawDropDown() {
 var ddButton = "<line x1='35%' y1='35%' x2='65%' y2='" + 35 + 30*closedOrOpen + "%' style='stroke-width:5;stroke:rgb(255,255,255);'/><line x1='" + 35 + 15*closedOrOpen + "%' y1='50%' x2='" + 65 - 15*closedOrOpen + "%' y2='50%' style='stroke-width:5;stroke:rgb(255,255,255);'/><line x1='35%' y1='65%' x2='65%' y2='" + 65 - 30*closedOrOpen + "%' style='stroke-width:5;stroke:rgb(255,255,255);'/>"
 document.getElementById('dd').innerHTML = ddButton
-}; */
+};
