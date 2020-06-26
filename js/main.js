61c3d85013e7efea9043c944de959d7af121b324
GreenCat <tedd0328@gmail.com>
2020-06-25 20:09:10 -0400

    Change Java stuffs idk

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var sz = screen.height*0.12;
-var ddButton = 0
-let ddButton = "<svg width='" + sz + "px'> <line x1='25%' y1='25%' x2='75%' y2='25%' style='stroke-width:2;stroke:rgb(255,255,255);'/><line x1='25%' y1='50%' x2='75%' y2='50%' style='stroke-width:2;stroke:rgb(255,255,255);'/><line x1='25%' y1='100%' x2='75%' y2='100%' style='stroke-width:2;stroke:rgb(255,255,255);'/></svg>";
-document.getElementById('dd').innerHTML = ddButton;
+var sz = screen.height*0.07;
+document.getElementById("dd").setAttribute("width", sz); 
+document.getElementById("dd").setAttribute("height", sz); 
+console.log(sz);
