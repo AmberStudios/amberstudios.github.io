e1c66342e37c5d6808d1dec9da186bef88d38971
Heccing Commie <51051125+Turtlz444@users.noreply.github.com>
2020-06-25 19:55:22 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,4 @@ var sz = screen.height*0.07;
 document.getElementById("dd").setAttribute("width", sz); 
 document.getElementById("dd").setAttribute("height", sz); 
 console.log(sz);
+document.getElementById("headerLogo").style.height = sz * 0.7
