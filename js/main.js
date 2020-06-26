817af3d22f692cd43d76ca6e887f9670b36cf745
GreenCat <tedd0328@gmail.com>
2020-06-26 17:28:23 -0400

    opsies

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var closedOrOpen = 0;
-var sz = screen.height * 0.07;
+var sz = screen.height * 0.1;
 
 document.getElementById('dd').setAttribute("width", sz); 
 document.getElementById('dd').setAttribute("height", sz);
