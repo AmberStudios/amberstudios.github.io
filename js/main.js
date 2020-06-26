015c8720717d484b3551078db45dfff44c6802af
Heccing Commie <51051125+Turtlz444@users.noreply.github.com>
2020-06-25 22:29:56 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ document.getElementById("dd").setAttribute("height", sz);
 console.log(sz);
 var closedOrOpen = 1;
 document.getElementById("headerLogo").style.height = sz * 0.7;
-window.onload = function drawDropDown() {
-var ddButton = document.getElementById('dd').innerHTML;
-let ddButton = "<svg id='dd'><line x1='35%' y1='35%' x2='" + 65 - closedOrOpen*30 + "%' y2='35%' style='stroke-width:5;stroke:rgb(255,255,255);'/><line x1='" + 35 + 15*closedOrOpen + "%' y1='50%' x2='" + 65 - 15*closedOrOpen + "%' y2='50%' style='stroke-width:5;stroke:rgb(255,255,255);'/><line x1='35%' y1='65%' x2='65%' y2='" + 65 - 30*closedOrOpen + "%' style='stroke-width:5;stroke:rgb(255,255,255);'/></svg>";
+function drawDropDown() {
+var ddButton = "<svg id='dd'><line x1='35%' y1='35%' x2='" + 65 - closedOrOpen*30 + "%' y2='35%' style='stroke-width:5;stroke:rgb(255,255,255);'/><line x1='" + 35 + 15*closedOrOpen + "%' y1='50%' x2='" + 65 - 15*closedOrOpen + "%' y2='50%' style='stroke-width:5;stroke:rgb(255,255,255);'/><line x1='35%' y1='65%' x2='65%' y2='" + 65 - 30*closedOrOpen + "%' style='stroke-width:5;stroke:rgb(255,255,255);'/></svg>"
+document.getElementById('dd').innerHTML = ddButton
 };
+window.onload = drawDropDown();
