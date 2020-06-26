27d7f80a0416541c6e24267c3701fd3a8febe336
Heccing Commie <51051125+Turtlz444@users.noreply.github.com>
2020-06-26 12:12:33 -0500

    Moved the percent

--- js/main.js
+++ js/main.js
@@ -8,13 +8,13 @@ document.getElementById("headerLogo").style.height = sz * 0.7;
 
 /* - dropdown button function - */
 function drawDropDown() {
-var a = 35 + 30*closedOrOpen;
-var b = 35 + 15*closedOrOpen;
-var c = 65 - 15*closedOrOpen;
-var d = 65 - 30*closedOrOpen;
-document.getElementById('l1').setAttribute('y2', a + '%');
-document.getElementById('l2').setAttribute('x1', b  + '%');
-document.getElementById('l2').setAttribute('x2', c  + '%');
-document.getElementById('l3').setAttribute('y2', d  + '%');
+var a = (35 + 30*closedOrOpen)  + '%';
+var b = (35 + 15*closedOrOpen)  + '%';
+var c = (65 - 15*closedOrOpen)  + '%';
+var d = (65 - 30*closedOrOpen)  + '%';
+document.getElementById('l1').setAttribute('y2', a);
+document.getElementById('l2').setAttribute('x1', b);
+document.getElementById('l2').setAttribute('x2', c);
+document.getElementById('l3').setAttribute('y2', d);
 };
 drawDropDown();
