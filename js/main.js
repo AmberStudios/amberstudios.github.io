c60f43f68594e6760d94ef54c42f55c85b5a2c4d
Heccing Commie <51051125+Turtlz444@users.noreply.github.com>
2020-06-27 14:08:54 -0500

    Added overlay

--- js/main.js
+++ js/main.js
@@ -12,12 +12,14 @@ if (closedOrOpen == 1) {
 	document.getElementById('l2').setAttribute('class', 'open');
 	document.getElementById('l3').setAttribute('class', 'open');
 	document.getElementById('dropdownTab').setAttribute('class', 'open');
+	document.getElementById('overlay').setAttribute('class', 'open');
 }
 if (closedOrOpen == 0) {
 	document.getElementById('l1').removeAttribute("class");
 	document.getElementById('l2').removeAttribute("class");
 	document.getElementById('l3').removeAttribute("class");
 	document.getElementById('dropdownTab').removeAttribute("class");
+	document.getElementById('overlay').removeAttribute("class");
 }
 
 };
