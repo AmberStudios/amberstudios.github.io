9a4513c551b36623656fa1ed39c7403fdbf7ae92
GreenNeon <68599923+NextRank@users.noreply.github.com>
2021-01-06 13:53:05 -0500

    Update pageInit.js

--- js/pageInit.js
+++ js/pageInit.js
@@ -1,3 +1,15 @@
 //A simple script that loads things that are universal on all pages.
 
-document.write(/legacyHeader.html);
+var navBlock = document.createElement( 'div' );
+
+var url = "http://amberstudios.github.io/legacyHeader.html";
+var xmlHTTP = new XMLHttpRequest();
+
+xmlHTTP.open("GET", url, false);
+xmlHTTP.send();
+
+navblock.innerHTML = xmlHTTP.responseText;
+
+containBlock = document.getElementById( 'navContainer' )
+
+containblock.appendChild(navBlock)
