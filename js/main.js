4fb5c5e72dcf0b4a8f90fa58031501f41065280f
Mast3rGenius <50230324+Mast3rGenius@users.noreply.github.com>
2021-04-23 20:04:21 -0600

    what a mess

--- js/main.js
+++ js/main.js
@@ -6,22 +6,21 @@ document.getElementById('dd').setAttribute("height", sz);
 console.log(sz);
 
 function switchDropDown() {
-closedOrOpen = 1 - closedOrOpen;
-if (closedOrOpen == 1) {
-	document.getElementById('l1').setAttribute('class','open');
-	document.getElementById('l2').setAttribute('class', 'open');
-	document.getElementById('l3').setAttribute('class', 'open');
-	document.getElementById('dropdownTab').setAttribute('class', 'open');
-	document.getElementById('overlay').setAttribute('class', 'open');
-	document.getElementById('overlay').style.display = "block";
-}
-if (closedOrOpen == 0) {
-	document.getElementById('l1').removeAttribute("class");
-	document.getElementById('l2').removeAttribute("class");
-	document.getElementById('l3').removeAttribute("class");
-	document.getElementById('dropdownTab').removeAttribute("class");
-	document.getElementById('overlay').removeAttribute("class");
-	document.getElementById('overlay').style.display = "none";
-}
-
+	closedOrOpen = 1 - closedOrOpen;
+	if (closedOrOpen == 1) {
+		document.getElementById('l1').setAttribute('class','open');
+		document.getElementById('l2').setAttribute('class', 'open');
+		document.getElementById('l3').setAttribute('class', 'open');
+		document.getElementById('dropdownTab').setAttribute('class', 'open');
+		document.getElementById('overlay').setAttribute('class', 'open');
+		document.getElementById('overlay').style.display = "block";
+	}
+	if (closedOrOpen == 0) {
+		document.getElementById('l1').removeAttribute("class");
+		document.getElementById('l2').removeAttribute("class");
+		document.getElementById('l3').removeAttribute("class");
+		document.getElementById('dropdownTab').removeAttribute("class");
+		document.getElementById('overlay').removeAttribute("class");
+		document.getElementById('overlay').style.display = "none";
+	}
 };
